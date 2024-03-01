d94eaf08aa953ae4eae8ac1455b0521d5da5af4e
NineTears <hellowind@live.com>
2024-03-01 09:36:40 +0800

    跟随Relar升级

--- Core.lua
+++ Core.lua
@@ -385,7 +385,7 @@ BigWigs.cmdtable = {type = "group", handler = BigWigs, args = {
 }}
 BigWigs:RegisterChatCommand({"/bw", "/BigWigs"}, BigWigs.cmdtable)
 BigWigs.debugFrame = ChatFrame1
-BigWigs.revision = 30058
+BigWigs.revision = 30059
 
 
 function BigWigs:DebugMessage(msg, module)
